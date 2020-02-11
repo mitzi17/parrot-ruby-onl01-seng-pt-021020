eaffeff34eca54a64d81fcc5c383f9852991fbf2
Mitzi Estrada <m_estrada17@live.com>
2020-02-11 06:30:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+ def parrot(name, name = "Squawk!")
+   puts "My name is #{name}"
+   return "My name is Squawk!"
+ end
+ 
